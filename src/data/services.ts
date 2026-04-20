f7f4b578cfd79935f67be4328f5aaad114bd745d
CoreMedica <dcads.truro@gmail.com>
2026-04-20 12:30:57 +0100

    Additions of new SCSS and header changes

---
+++ src/data/services.ts
@@ -0,0 +1,29 @@
+export const services = [
+  {
+    name: "Ear Care",
+    slug: "ear-care",
+    status: "live",
+    children: [
+      {
+        name: "Portsmouth",
+        slug: "portsmouth",
+        status: "live",
+      },
+      {
+        name: "Southampton",
+        slug: "southampton",
+        status: "live",
+      },
+    ],
+  },
+  {
+    name: "Men's Health",
+    slug: "mens-health",
+    status: "planned",
+  },
+  {
+    name: "Women's Health",
+    slug: "womens-health",
+    status: "planned",
+  },
+];
