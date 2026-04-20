cde4a2b6d30bad2c5f7cce0cae1cba7031f4061f
CoreMedica <dcads.truro@gmail.com>
2026-04-20 08:12:15 +0100

    Setting up and adding main pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.promedicowellnessgroup.co.uk",
+  output: "static",
+  integrations: [react(), sitemap()],
+  trailingSlash: "never",
+});
