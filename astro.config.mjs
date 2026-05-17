d332ea2b92a2b7b3b52f167407004a5fd873856e
CoreMedica <dcads.truro@gmail.com>
2026-05-17 05:38:23 +0100

    Update site URL in configuration to reflect the correct domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://www.promedicowellnessgroup.co.uk",
+  site: "https://promedico.co.uk",
   output: "static",
   integrations: [react(), sitemap()],
   trailingSlash: "never",
